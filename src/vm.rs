636a508064fcbd802221a0641ded826817340a6d
Guillaume Malette <guillaume@shopify.com>
2017-02-13 21:02:06 -0500

    Call "works"

--- src/vm.rs
+++ src/vm.rs
@@ -11,8 +11,7 @@ pub enum Value {
     CharString(String),
     Boolean(bool),
     Map(BTreeMap<Rc<Value>, Rc<Value>>),
-    Closure(u8, Closure),
-    Fn(u8, Closure),
+    Closure(Box<Vec<String>>, Closure),
 }
 
 #[derive(Clone, Eq, Debug, PartialEq)]
@@ -21,23 +20,72 @@ pub enum Instruction {
     Fetch(String),
     LocalAssign(String),
     Assign(String),
-    Call(u8),
+    Call(usize),
+    Nop,
     // Set(String, Value),
 }
 
 pub type InstructionSequence = Vec<Instruction>;
-pub type BindingMap = Rc<RefCell<BTreeMap<String, Value>>>;
+
+#[derive(Clone, Eq, Debug, PartialEq)]
+pub struct BindingMap {
+    map: Rc<RefCell<BTreeMap<String, Value>>>,
+    parent: Option<Box<BindingMap>>,
+}
+
+impl BindingMap {
+    fn new(parent: Option<&BindingMap>) -> BindingMap {
+        let parent = match parent {
+            Some(map) => Some(Box::new(map.clone())),
+            None => None,
+        };
+        BindingMap {
+            map: Rc::new(RefCell::new(BTreeMap::new())),
+            parent: parent,
+        }
+    }
+
+    pub fn fetch(&self, binding_name: &String) -> Option<Value> {
+        match self.map.borrow().get(binding_name) {
+            Some(value) => Some(value.clone()),
+            None => {
+                match self.parent {
+                    Some(ref parent) => parent.fetch(binding_name),
+                    None => None,
+                }
+            },
+        }
+    }
+
+    pub fn local_assign(&mut self, binding_name: &String, value: Value) {
+        self.map.borrow_mut().insert(binding_name.to_owned(), value);
+    }
+
+    pub fn assign(&mut self, binding_name: &String, value: Value) {
+        match self.has_binding(binding_name) {
+            true => self.local_assign(binding_name, value),
+            false => match self.parent {
+                Some(ref mut parent) => parent.assign(binding_name, value),
+                None => panic!("no such binding"),
+            }
+        }
+    }
+
+    pub fn has_binding(&self, binding_name: &String) -> bool {
+        self.map.borrow().contains_key(binding_name)
+    }
+}
 
 #[derive(Clone, Eq, Debug, PartialEq)]
 pub struct Closure {
-    instructions: Box<InstructionSequence>,
+    instructions: Rc<InstructionSequence>,
     parent_bindings: BindingMap,
 }
 
 impl Closure {
     pub fn new(instructions: InstructionSequence, parent_bindings: &BindingMap) -> Closure {
         Closure {
-            instructions: Box::new(instructions),
+            instructions: Rc::new(instructions),
             parent_bindings: parent_bindings.clone(),
         }
     }
@@ -50,15 +98,11 @@ struct Frame {
 }
 
 impl Frame {
-    pub fn new() -> Frame {
+    pub fn new(bindings: BindingMap) -> Frame {
         Frame {
-            bindings: Rc::new(RefCell::new(BTreeMap::new())),
+            bindings: bindings,
             exception_handlers: Vec::new(),
         }
-    }
-
-    pub fn local_assign(&mut self, binding_name: &str, value: Value) {
-        self.bindings.borrow_mut().insert(binding_name.to_owned(), value);
     }
 }
 
@@ -79,7 +123,7 @@ fn compile_statement<'a>(statement: &'a Statement) -> InstructionSequence {
                 .flat_map(|exp| compile_expression(exp))
                 .collect::<InstructionSequence>();
             instructions.push(Instruction::Fetch(binding_name.to_owned()));
-            instructions.push(Instruction::Call(expressions.len() as u8));
+            instructions.push(Instruction::Call(expressions.len()));
             instructions
         },
         // _ => panic!("not implemented"),
@@ -121,12 +165,11 @@ pub struct Vm {
 impl Vm {
     pub fn new(source: &str) -> Vm {
         let stmts = statements(source);
-        // println!("{:?}", stmts);
         let instructions = compile(&stmts.unwrap());
-        // println!("{:?}", instructions);
-        let mut frame = Frame::new();
+        let mut map = BindingMap::new(None);
+        let mut frame = Frame::new(map);
 
-        let mut vm = Vm{
+        let mut vm = Vm {
             instructions: Rc::new(instructions),
             pc: 0,
             stack: Vec::new(),
@@ -135,7 +178,7 @@ impl Vm {
         vm
     }
 
-    pub fn run<'b, 'c>(&'b mut self) {
+    pub fn run<'b>(&'b mut self) {
         loop {
             let insn_result = Vm::next_instruction(self);
             let instruction;
@@ -149,28 +192,41 @@ impl Vm {
                 Instruction::Push(ref value) => {
                     let top_bindings = &mut self.frames.last_mut().unwrap().bindings;
                     self.stack.push(Vm::literal_to_value(value, top_bindings))
-                    // Vm::push_value(&mut self.stack, top_activation, value)
+                },
+                Instruction::Assign(ref binding_name) => {
+                    let value = self.stack.pop().unwrap();
+                    self.frames.last_mut().unwrap().bindings.assign(binding_name, value)
                 },
                 Instruction::LocalAssign(ref binding_name) => {
                     let value = self.stack.pop().unwrap();
-                    self.frames.last_mut().unwrap().local_assign(binding_name, value)
+                    self.frames.last_mut().unwrap().bindings.local_assign(binding_name, value)
                 },
                 Instruction::Call(arg_size) => {
-                    let (closure_arg_size, closure) = match self.stack.pop() {
-                        Some(Value::Closure(arg_size, closure)) => (arg_size, closure),
-                        _ => panic!("expected a closure"),
+                    let (closure_args, closure) = match self.stack.pop() {
+                        Some(Value::Closure(arg_names, closure)) => (arg_names, closure),
+                        Some(x) => panic!("expected a closure, got {:?}", x),
+                        None => panic!("expected a closure, got None"),
                     };
-                    // if arg_size != closure_arg_size {
-                    //     panic!("wrong number of arguments")
-                    // };
-                    // make sure closure
-                    // verify args
-                    // push frame
-                    // make new activation
-                    // assign values
-                    // change iseq
+                    if arg_size != closure_args.len() {
+                        panic!(
+                            "wrong number of arguments, expected {:?}, got {:?}",
+                            closure_args.len(),
+                            arg_size
+                        )
+                    };
+                    let mut map = BindingMap::new(Some(&closure.parent_bindings));
+                    for arg_name in closure_args.iter().rev() {
+                        map.local_assign(arg_name, self.stack.pop().unwrap());
+                    }
+                    self.frames.push(Frame::new(map));
+                    self.instructions = closure.instructions.clone();
+                    self.pc = 0;
                 },
-                _ => ()//panic!("unknown instruction {:?}", instruction)
+                Instruction::Fetch(ref binding_name) => {
+                    let value = self.frames.last().unwrap().bindings.fetch(binding_name).unwrap();
+                    self.stack.push(value);
+                },
+                _ => panic!("unknown instruction {:?}", instruction),
             };
         };
     }
@@ -193,10 +249,77 @@ impl Vm {
             &Literal::Fn(ref args, ref statements) => {
                 // Statements should be compiled ahead of time
                 let closure = Closure::new(compile(&statements), top_bindings);
-                Value::Closure(args.len() as u8, closure)
+                Value::Closure(args.clone(), closure)
             },
             _ => panic!("not implemented"),
         }
+    }
+}
+
+#[cfg(test)]
+mod test_binding_map {
+    use super::*;
+    use std::collections::BTreeMap;
+    use std::rc::Rc;
+    use std::cell::RefCell;
+
+    #[test]
+    fn test_new() {
+        assert_eq!(
+            Rc::new(RefCell::new(BTreeMap::new())),
+            BindingMap::new(None).map
+        )
+    }
+
+    #[test]
+    fn test_has_binding() {
+        let mut map = BindingMap::new(None);
+        assert_eq!(
+            false,
+            map.has_binding(&"toto".to_owned())
+        )
+    }
+
+    #[test]
+    fn test_local_assign() {
+        let mut map = BindingMap::new(None);
+        map.local_assign(&"toto".to_owned(), Value::CharString("value".to_owned()));
+        assert!(map.has_binding(&"toto".to_owned()))
+    }
+
+    #[test]
+    fn test_fetch() {
+        let mut map = BindingMap::new(None);
+        map.local_assign(&"toto".to_owned(), Value::CharString("value".to_owned()));
+        assert_eq!(
+            Some(Value::CharString("value".to_owned())),
+            map.fetch(&"toto".to_owned())
+        )
+    }
+
+    #[test]
+    fn test_delegates_fetch() {
+        let mut parent = BindingMap::new(None);
+        parent.local_assign(&"toto".to_owned(), Value::CharString("value".to_owned()));
+        let map = BindingMap::new(Some(&parent));
+
+        assert_eq!(
+            Some(Value::CharString("value".to_owned())),
+            map.fetch(&"toto".to_owned())
+        )
+    }
+
+    #[test]
+    fn test_delegates_assign() {
+        let mut parent = BindingMap::new(None);
+        parent.local_assign(&"toto".to_owned(), Value::CharString("value".to_owned()));
+        let mut map = BindingMap::new(Some(&parent));
+        map.assign(&"toto".to_owned(), Value::CharString("new_value".to_owned()));
+
+        assert_eq!(
+            Some(Value::CharString("new_value".to_owned())),
+            parent.fetch(&"toto".to_owned())
+        )
     }
 }
 
@@ -221,8 +344,9 @@ mod test_vm {
         )
     }
 
+    #[test]
     fn test_new_populates_function_instructions() {
-        let vm = Vm::new("let b = def(x)\nend\nb(1)");
+        let vm = Vm::new("let b = def(x) do\nend\nb(1)");
         assert_eq!(
             Rc::new(vec![
                 Instruction::Push(Literal::Fn(Box::new(vec!["x".to_owned()]), Box::new(vec![]))),
@@ -241,16 +365,14 @@ mod test_vm {
         vm.run();
         assert_eq!(
             Value::Number(build_ratio(1, 1)),
-            vm.frames.last().unwrap().bindings.borrow().get("a").unwrap().to_owned()
-        );
-        ()
+            vm.frames.last().unwrap().bindings.fetch(&"a".to_owned()).unwrap().to_owned()
+        )
     }
 
     #[test]
-    #[ignore]
     fn test_function_call() {
         let source =
-            "let a = \"\"\
+        "let a = \"\"\
             let x = def() do
               a = 1
             end\
@@ -259,14 +381,37 @@ mod test_vm {
         let mut vm = Vm::new(source);
         vm.run();
         assert_eq!(
-            Value::Number(build_ratio(1, 1)),
-            vm.frames.last().unwrap().bindings.borrow().get("a").unwrap().to_owned()
-        );
-        ()
+        Value::Number(build_ratio(1, 1)),
+        vm.frames.last().unwrap().bindings.fetch(&"a".to_owned()).unwrap().to_owned()
+        )
     }
 
     #[test]
-    #[should_panic]
+    fn test_function_call_with_args() {
+        let source =
+            "\
+            let a = \"\"\
+            let b = \"\"\
+            let x = def(c, d) do
+              a = c
+              b = d
+            end\
+            x(1, 2)";
+
+        let mut vm = Vm::new(source);
+        vm.run();
+        assert_eq!(
+            Value::Number(build_ratio(1, 1)),
+            vm.frames.last().unwrap().bindings.fetch(&"a".to_owned()).unwrap().to_owned()
+        );
+        assert_eq!(
+            Value::Number(build_ratio(2, 1)),
+            vm.frames.last().unwrap().bindings.fetch(&"b".to_owned()).unwrap().to_owned()
+        )
+    }
+
+    #[test]
+    #[should_panic(expected="expected a closure")]
     fn test_calling_non_function() {
         let source =
             "let x = \"\"\
@@ -277,13 +422,12 @@ mod test_vm {
     }
 
     #[test]
-    #[should_panic]
+    #[should_panic(expected="wrong number of arguments")]
     fn test_function_with_wrong_arg_count() {
         let source =
             "let x = def(a, b) do
-              a = 1
             end\
-            x(1, 1)";
+            x(1)";
 
         let mut vm = Vm::new(source);
         vm.run();
