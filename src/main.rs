658abbb3969d49bf46ec1d905d3738c82d769536
Guillaume Malette <guillaume@shopify.com>
2017-03-06 23:22:33 -0500

    Add basic main file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,27 @@
 extern crate num;
 
+#[cfg(test)]
 #[macro_use]
 mod test_helpers;
+
 mod ast;
 mod grammar;
 mod vm;
+use std::env;
+use vm::Vm;
+
+fn exec(source: &String) {
+    let mut vm = Vm::new(source);
+    vm.run();
+}
 
 fn main() {
-    match grammar::statements(&"let a = 12345") {
-        Ok(v) => {
-            println!("got value {:?}", v);
-        }
-        Err(err) => panic!("parse error: {:?}", err),
+    if let Some(source) = env::args().take(2).last() {
+        println!("ARGV contains what could be source");
+        println!("Starting VM with ARGV");
+        println!("{}", source);
+        exec(&source);
+    } else {
+        println!("No args found, stopping");
     }
 }
